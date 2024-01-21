eaf1cab4d0667d7d9845f38f900bfe4e2026fd05
Dev-Dylann <tubitunmise@gmail.com>
2024-01-21 21:44:14 +0100

    implemented server side form validation

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 type Props = {
     params: {
@@ -11,6 +12,10 @@ type Props = {
 export default async function Page({ params }: Props) {
     const { id } = params
     const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()])
+
+    if (!invoice) {
+      return notFound()
+    }
 
   return (
     <main>
